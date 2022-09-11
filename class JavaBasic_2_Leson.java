dcde16e44571d30a2a3bfa5daf7c3fdccda587e0
samgomax <maxxxym300887@gmail.com>
2022-09-11 20:32:26 +0200

    Create class JavaBasic_2_Leson.java

---
+++ class JavaBasic_2_Leson.java
@@ -0,0 +1,9 @@
+class JavaBasic_2_Leson {
+    public static void main(String[] args) {
+        printHello();
+    }
+    
+    static void printHello() {
+        System.out.println("Hello");
+    }
+}
