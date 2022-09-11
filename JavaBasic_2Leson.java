38ce61787dd02636a888497e34a5586cb97022a3
samgomax <maxxxym300887@gmail.com>
2022-09-11 20:32:21 +0200

    Create JavaBasic_2Leson.java

---
+++ JavaBasic_2Leson.java
@@ -0,0 +1,8 @@
+class JavaBasic_2Leson {
+    public static void main(String[] args) {
+            int t = -4;
+        if (t < 0){
+            System.out.println("It's frost");
+        }
+    }
+}
