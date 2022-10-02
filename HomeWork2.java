b7cee2ad1fb312d4af3ce9d85e12317dd22586ff
Maksym_S <112724260+samgomax@users.noreply.github.com>
2022-10-02 16:12:29 +0200

    HomeWork2 - Upload

--- HomeWork2.java
+++ HomeWork2.java
@@ -2,57 +2,62 @@
  *Java Basic. Home work - 2. Methods
  *
  * @author Maksym Samohorodskyi
- * @todo 11.09
- * @date 11.09
- *
- */
+ * @todo 07.09
+ * @date 02.10
+**/
 class HomeWork2 {
-    public static void main(String[] args){
-        int c = add(15, 5);
-        System.out.println(add(15, 5));
-        int d = sub(15, 5);
-        System.out.println(sub(15, 5));
-        int e = mult(15, 5);
-        System.out.println(mult(15, 5));
-        int f = div(15, 5);
-        System.out.println(div(15, 5));
-        
-        int t = -8;
-        if (t < 0){
-            System.out.println("It's frost");
-        } else if (t >= 0 && t <= 18){
-            System.out.println("It's cold");
-        } else if (t >= 19 && t <= 28){
-            System.out.println("It's warm");
-        } else if (t > 29){
-            System. out.println("It's hot");
-/* я никак не могу понять как мне правильно написать метод и вернуть строку.
-постоянно какая-то ошибка при попытку записать через switch, выводит ошибку,
-что нельзя преобразовать int в boolean*/
-        }
-//** exercise 2
-        boolean isWeekend = false;
-        boolean isRain = false;
-        if(!isWeekend && !isRain) {
-            System.out.println("I can't walk");
+    public static void main(String[] args) {
+        //task 1
+        int temperature = 15;
+        System.out.println(checkWeather(-5));
+        System.out.println(checkWeather(5));
+        System.out.println(checkWeather(25));
+        System.out.println(checkWeather(35));
+
+        //task 2
+        System.out.println(canWalk(true, true)? "I can walk" : "I can't walk");
+        System.out.println(canWalk(false, true)? "I can walk" : "I can't walk");
+        System.out.println(canWalk(true, false)? "I can walk" : "I can't walk");
+        System.out.println(canWalk(false, false)? "I can walk" : "I can't walk");
+
+        //task 3
+        int a = 5;
+        int b = 3;
+        System.out.printf("%d + %d = %d\n", a, b, add(a, b));
+        System.out.printf("%d - %d = %d\n", a, b, sub(a, b));
+        System.out.printf("%d * %d = %d\n", a, b, mul(a, b));
+        System.out.printf("%d / %d = %f\n", a, b, div(a, b));
+    }
+
+    static String checkWeather(int temperature){
+        if(temperature < 0) {
+            return "It's a frost";
+        } else if (temperature <= 18) {
+            return "It's cold";
+        }else if (temperature <=28) {
+            return "it's warm";
+        }else {
+            return "It's hot";
         }
     }
-//*exercise 3 
-        static int add(int a, int b){
-            int c = a+b;
-            return c;
-        }
-        static int sub(int a, int b){
-            int d = a-b;
-            return d;
-        }
-        static int mult(int a, int b){
-            int e = a*b;
-            return e;
-        }
-        static int div(int a, int b){
-            int f = a/b;
-            return f;
-        }
+
+    static boolean canWalk(boolean isWeekend, boolean isRain) {
+        return isWeekend && !isRain;
     }
-//* я не могу понять логику последовательности действий. 
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int sub(int a, int b) {
+        return a - b;
+    }
+
+    static int mul(int a, int b) {
+        return a * b;
+    }
+
+    static float  div(float a, float b) {
+        return a / b;
+    }
+}
