4f960066236a9792af897dfd4bca677272b19b3f
Maksym_S <112724260+samgomax@users.noreply.github.com>
2022-10-02 14:39:05 +0200

    8 Lesson

---
+++ JavaBasic8Lesson.java
@@ -0,0 +1,23 @@
+/**
+ *Java Basic. Home work - 8.
+ *
+ * @author Maksym Samohorodskyi
+ * @todo 28.09
+ * @date 02.10
+ **/
+public class JavaBasic8Lesson {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik","red" ,2, 1);
+        System.out.println(cat);
+        System.out.println(cat.voice());
+        System.out.println("Try to play: " + cat.play());
+        cat.feed();
+        System.out.println("Try to play: " + cat.play());
+        System.out.println("Try to play: " + cat.play());
+        cat.feed();
+        System.out.println("Try to jump: " + cat.jump(1));
+        cat.feed();
+        System.out.println("Try to jump: " + cat.jump(2));
+    }
+}
+
