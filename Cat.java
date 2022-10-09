1bb794bc19078137a19a7e1ad1a459843f021a5d
Maksym_S <112724260+samgomax@users.noreply.github.com>
2022-10-09 14:04:03 +0200

    Add files via upload
    JavaBasic9Lesson

--- Cat.java
+++ Cat.java
@@ -1,59 +1,10 @@
-/**
- *Java Basic. Home work - 8.
- * *
- * @author Maksym Samohorodskyi
- * @todo 28.09
- * @date 02.10
- **/
-public class Cat {
-    String name;
-    String color;
-    int age;
-    int jumpMax;
-    boolean full;
-
-    public Cat(String name, String color, int age, int jumpMax) {
-        this.name = name;
-        this.color = color;
-        this.age = age;
-        this.jumpMax = jumpMax;
-        this.full = false;
+public class Cat extends Animal implements IAnimal{
+    public Cat(String name, String color, int age) {
+        super(name, color, age);
     }
 
-    public void feed() {
-        full = true;
-    }
-
-    public  boolean play() {
-        if (full) {
-            full = false;
-            return true;
-        } else {
-            System.out.println(voice());
-            return false;
-        }
-    }
-
-    public boolean jump( int height){
-        if (full) {
-            full = false;
-            if (height <= jumpMax) {
-                return true;
-            } else {
-                System.out.println(voice());
-                return false;
-            }
-        } else {
-            System.out.println(voice());
-            return false;
-        }
-    }
-
+    @Override
     public String voice() {
-        return  name + ": meow";
-    }
-
-    public String toString() {
-       return "Cat: " + name + ", color:" + color + ", age :" + age;
+        return "meow";
     }
 }
---
+++ javaBasic9Lesson.java
@@ -0,0 +1,20 @@
+/**
+ *Java Basic. Home work - 3.
+ *
+ * @author Maksym Samohorodskyi
+ * @todo 05.10
+ * @date 09.10
+ **/
+
+public class javaBasic9Lesson {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik","red" ,2);
+        Dog dog = new Dog("Polkan", "black", 5 );
+        IAnimal[] animals = {cat, dog};
+
+        for (IAnimal animal : animals) {
+            System.out.println(animal);
+            System.out.println(animal.voice());
+        }
+    }
+}
